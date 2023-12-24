039d8db8ca8fddc6e3624bf211307be9feee3229
dante <guchoo923@gmail.com>
2023-12-24 11:38:54 +0200

    fix type definitions, add functionality to adding a new user, make a services folder

---
+++ client/src/types.tsx
@@ -0,0 +1,32 @@
+
+type Address = {
+  [K in 
+    "country" |
+    "city" |
+    "street" |
+    "streetNumber"
+  ]: string;
+};
+
+export type NewUserData = {
+    [K in
+      "firstName" |
+      "lastName" | 
+      "email" |
+      "phoneNumber" |
+      "imgUrl"
+    ]: string;
+} 
+  &
+{ 
+  address: Address
+};
+
+export type UserData = NewUserData & ServerDefaultData 
+
+type ServerDefaultData = {
+  "_id": string,
+  "createdAt": string
+}
+
+export type SetStateFunction = React.Dispatch<React.SetStateAction<UserData[]>>
